ceb675d7cae01d8a43cc75f1534b01fe0a8844fc
SebaDro <sebastian.drost@hs-bochum.de>
2019-05-17 14:37:38 +0200

    Add updater for GdiDeDataEnvelopes

---
+++ metadata-listener/src/main/java/de/wacodis/metadataconnector/inspect/GdiDeMetadataUpdater.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.wacodis.metadataconnector.inspect;
+
+import de.wacodis.metadataconnector.model.AbstractDataEnvelope;
+import de.wacodis.metadataconnector.model.GdiDeDataEnvelope;
+
+/**
+ * MetadataUpdater for {@link GdiDeDataEnvelope} instances
+ * @author <a href="mailto:s.drost@52north.org">Sebastian Drost</a>
+ */
+public class GdiDeMetadataUpdater extends AbstractMetadataUpdater<GdiDeDataEnvelope> {
+
+    @Override
+    public boolean supportsDataEnvelope(AbstractDataEnvelope dataEnvelope) {
+        return dataEnvelope instanceof GdiDeDataEnvelope;
+    }
+
+    @Override
+    public AbstractDataEnvelope updateDataEnvelope(GdiDeDataEnvelope existingDataEnvelope, GdiDeDataEnvelope newDataEnvelope) {
+        existingDataEnvelope.setAreaOfInterest(
+                this.mergeAreaOfInterest(
+                        existingDataEnvelope.getAreaOfInterest(),
+                        newDataEnvelope.getAreaOfInterest()));
+
+        existingDataEnvelope.setCreated(
+                this.mergeCreated(
+                        existingDataEnvelope.getCreated(),
+                        newDataEnvelope.getCreated()));
+
+        existingDataEnvelope.setModified(
+                this.mergeCreated(
+                        existingDataEnvelope.getModified(),
+                        newDataEnvelope.getModified()));
+
+        existingDataEnvelope.setTimeFrame(
+                this.mergeTimeFrame(
+                        existingDataEnvelope.getTimeFrame(),
+                        newDataEnvelope.getTimeFrame()));
+
+        return existingDataEnvelope;
+    }
+
+}
