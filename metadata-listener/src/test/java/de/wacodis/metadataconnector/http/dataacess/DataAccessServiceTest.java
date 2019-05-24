15d5bf1c8da51073b92611d0e7f15eacf4038853
SebaDro <sebastian.drost@hs-bochum.de>
2019-05-24 14:36:56 +0200

    Expand test for DataAccessService

--- metadata-listener/src/test/java/de/wacodis/metadataconnector/http/dataacess/DataAccessServiceTest.java
+++ metadata-listener/src/test/java/de/wacodis/metadataconnector/http/dataacess/DataAccessServiceTest.java
@@ -15,10 +15,8 @@ import de.wacodis.metadataconnector.model.SensorWebDataEnvelope;
 import java.util.Arrays;
 import org.joda.time.DateTime;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
@@ -26,7 +24,6 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.client.ExpectedCount;
 import org.springframework.test.web.client.MockRestServiceServer;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.*;
@@ -62,10 +59,7 @@ public class DataAccessServiceTest {
     public void init() {
         expEnv = prepareEnvelope();
         testEnv = prepareEnvelope();
-    }
 
-    @Test
-    public void testSearchDataEnvelopeWithSuccess() throws JsonProcessingException, DataAccessRequestException {
         AbstractDataEnvelopeAreaOfInterest testAoi = new AbstractDataEnvelopeAreaOfInterest();
         testAoi.setExtent(Arrays.asList(7.0001f, 52.0001f, 7.0002f, 52.0002f));
         testEnv.setAreaOfInterest(testAoi);
@@ -74,7 +68,7 @@ public class DataAccessServiceTest {
         testTimeFrame.setStartTime(DateTime.parse("2019-01-02T00:00Z"));
         testTimeFrame.setEndTime(DateTime.parse("2019-01-03T00:00Z"));
         testEnv.setTimeFrame(testTimeFrame);
-        
+
         AbstractDataEnvelopeAreaOfInterest expAoi = new AbstractDataEnvelopeAreaOfInterest();
         expAoi.setExtent(Arrays.asList(7.0000f, 52.0000f, 7.0003f, 52.0003f));
         expEnv.setAreaOfInterest(expAoi);
@@ -83,7 +77,10 @@ public class DataAccessServiceTest {
         expTimeFrame.setStartTime(DateTime.parse("2019-01-01T00:00Z"));
         expTimeFrame.setEndTime(DateTime.parse("2019-01-4T00:00Z"));
         expEnv.setTimeFrame(expTimeFrame);
-        
+    }
+
+    @Test
+    public void testSearchDataEnvelopeWithSuccess() throws JsonProcessingException, DataAccessRequestException {
         mockServer.expect(ExpectedCount.once(),
                 requestTo("/dataenvelopes/search"))
                 .andExpect(method(HttpMethod.POST))
@@ -97,8 +94,24 @@ public class DataAccessServiceTest {
         Assertions.assertEquals(expEnv, result);
     }
 
+    @Test
+    public void testUpdateDataEnvelopeWithSuccess() throws JsonProcessingException, DataAccessRequestException {
+        mockServer.expect(ExpectedCount.once(),
+                requestTo("/dataenvelopes/" + testEnv.getIdentifier()))
+                .andExpect(method(HttpMethod.PUT))
+                .andRespond(withStatus(HttpStatus.OK)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(mapper.writeValueAsString(testEnv))
+                );
+        AbstractDataEnvelope result = service.updateDataEnvelope(testEnv);
+
+        mockServer.verify();
+        Assertions.assertEquals(testEnv, result);
+    }
+
     private SensorWebDataEnvelope prepareEnvelope() {
         SensorWebDataEnvelope envelope = new SensorWebDataEnvelope();
+        envelope.setIdentifier("123Test");
         envelope.setOffering("testOffering");
         envelope.setObservedProperty("testProperty");
         envelope.setFeatureOfInterest("testFeature");
